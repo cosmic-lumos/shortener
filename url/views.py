684b7c7749f34ebed3884e29fe5bcf1e6509843a
bitesnail <rkwoal216@gmail.com>
2023-09-14 13:21:58 +0900

    feat: url단축 뷰와 path 추가

--- url/views.py
+++ url/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from url.forms import UrlForm
+from url.models import Url
 
 # Create your views here.
-def get_shortened_url(request, url):
+def get_shortened_url(request, shortened):
+    url = Url.objects.filter(shortened__exact=shortened).get()
     
-    if url == "cbnu":
-        result = "https://chungbuk.ac.kr"
-    else:
-        result = "Not Found"
+    if url == None:
+        return redirect(show_home)
     
-    return render(request, 'index.html', {"url": result})
+    return redirect(str(url.link))
 
 def show_home(request):
-    
-    return render(request, 'url/index.html')
+    if request.method == 'POST':
+        form = UrlForm(request.POST)
+        url = form.save(commit=False)
+        url.shortened = url.link[0:5]
+        url.save()
+        return render(request, 'url/shorten.html', {"url": url.shortened})
+        
+    url_form = UrlForm()
+    return render(request, 'url/index.html', {"form": url_form})
